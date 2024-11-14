f69e9201924b9cc7c55b01d94afd4a49d9ba47c5
skdltn210 <rhdmstn202@gmail.com>
2024-11-14 15:26:45 +0900

    🐛 Fix: user 타입 설정

--- Frontend/src/store/useAuthStore.ts
+++ Frontend/src/store/useAuthStore.ts
@@ -1,10 +1,18 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
+interface User {
+  id: number;
+  nickname: string;
+  liveId: number | null;
+  profileImage?: string;
+  channelId: string | null;
+}
+
 interface AuthState {
   accessToken: string | null;
-  user: any | null;
-  setAuth: (auth: { accessToken: string; user: any }) => void;
+  user: User | null;
+  setAuth: (auth: { accessToken: string; user: User }) => void;
   clearAuth: () => void;
 }
 
