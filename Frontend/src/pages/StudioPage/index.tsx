57e66b51015955b290c95358e030d4dd9bedaa10
skdltn210 <rhdmstn202@gmail.com>
2024-11-14 14:50:16 +0900

    🐛 Fix: 사용하지 않는 useEffect 삭제

--- Frontend/src/pages/StudioPage/index.tsx
+++ Frontend/src/pages/StudioPage/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useLayoutStore from '@store/useLayoutStore';
 // import { useAuth } from '@hooks/useAuth';
