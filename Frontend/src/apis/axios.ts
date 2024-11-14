c08afdff42e0066538945e198370a25c5c6c2e02
skdltn210 <rhdmstn202@gmail.com>
2024-11-14 15:02:27 +0900

    🐛 Fix: axios 인터셉터 주석처리

--- Frontend/src/apis/axios.ts
+++ Frontend/src/apis/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { config } from '@/config/env';
-import { useAuthStore } from '@/store/useAuthStore';
+// import { useAuthStore } from '@/store/useAuthStore';
 // import { RefreshTokenResponse } from '@/types/auth';
 
 export const api = axios.create({
@@ -12,36 +12,36 @@ export const api = axios.create({
   withCredentials: true,
 });
 
-api.interceptors.request.use(config => {
-  const accessToken = useAuthStore.getState().accessToken;
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`;
-  }
-  return config;
-});
+// api.interceptors.request.use(config => {
+//   const accessToken = useAuthStore.getState().accessToken;
+//   if (accessToken) {
+//     config.headers.Authorization = `Bearer ${accessToken}`;
+//   }
+//   return config;
+// });
 
-api.interceptors.response.use(
-  response => response,
-  async error => {
-    // const originalRequest = error.config;
+// api.interceptors.response.use(
+//   response => response,
+//   async error => {
+// const originalRequest = error.config;
 
-    // if (error.response?.status === 401 && !originalRequest._retry) {
-    //   originalRequest._retry = true;
+// if (error.response?.status === 401 && !originalRequest._retry) {
+//   originalRequest._retry = true;
 
-    //   try {
-    //     const response = await api.post<RefreshTokenResponse>('/auth/refresh');
-    //     const { accessToken, user } = response.data;
+//   try {
+//     const response = await api.post<RefreshTokenResponse>('/auth/refresh');
+//     const { accessToken, user } = response.data;
 
-    //     useAuthStore.getState().setAuth({ accessToken, user });
-    //     originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-    //     return api(originalRequest);
-    //   } catch (refreshError) {
-    //     useAuthStore.getState().clearAuth();
-    //     window.location.href = '/login';
-    //     return Promise.reject(refreshError);
-    //   }
-    // }
+//     useAuthStore.getState().setAuth({ accessToken, user });
+//     originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+//     return api(originalRequest);
+//   } catch (refreshError) {
+//     useAuthStore.getState().clearAuth();
+//     window.location.href = '/login';
+//     return Promise.reject(refreshError);
+//   }
+// }
 
-    return Promise.reject(error);
-  },
-);
+//     return Promise.reject(error);
+//   },
+// );
